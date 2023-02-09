8d7446d775b9a78f16e9881377e826f1f6e5abf3
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-02-09 07:15:14 -0300

    mockito

---
+++ backend/src/test/java/com/heroes/dscatalog/services/ProductServiceTest.java
@@ -0,0 +1,21 @@
+package com.heroes.dscatalog.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.heroes.dscatalog.repositories.ProductRepository;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTest {
+	
+	@InjectMocks
+	private ProductService service;
+	
+	@Mock
+	private ProductRepository repository;
+	
+	
+
+}
