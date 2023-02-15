df9023310097ada4be94768f44749bc0e68361be
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-02-15 08:04:44 -0300

    integrations tests BD

--- backend/src/test/java/com/heroes/dscatalog/services/ProductServicesIT.java
+++ backend/src/test/java/com/heroes/dscatalog/services/ProductServicesIT.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.heroes.dscatalog.dto.ProductDTO;
 import com.heroes.dscatalog.repositories.ProductRepository;
-import com.heroes.dscatalog.services.exceptions.DatabaseException;
 import com.heroes.dscatalog.services.exceptions.ResourceNotFoundExecption;
 
 @SpringBootTest
+@Transactional
 public class ProductServicesIT {
 	
 	@Autowired
@@ -49,5 +54,50 @@ public class ProductServicesIT {
 		
 	}
 	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPage0Size10() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result =  service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+		
+	}
+	
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result =  service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+		
+		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnOrderedPageWhenPageSortByName() {
+		
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result =  service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		
+	}
 	
 }
+
+
+
+
+
+
