59562513dda4251817fe06e24a03dc6c7e632088
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-02-16 07:27:50 -0300

    test integration update

--- backend/src/test/java/com/heroes/dscatalog/resources/ProductResourceIT.java
+++ backend/src/test/java/com/heroes/dscatalog/resources/ProductResourceIT.java
@@ -1,6 +1,7 @@
 package com.heroes.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.heroes.dscatalog.dto.ProductDTO;
+import com.heroes.dscatalog.tests.Factory;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -21,6 +26,9 @@ public class ProductResourceIT {
 	
 	@Autowired
 	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	private Long existingId;
 	private Long nonExistingId;
@@ -47,5 +55,28 @@ public class ProductResourceIT {
 		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
 		
 	}
+	
+	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdNonexists() throws Exception {
+		
+		ProductDTO productDTO = Factory.createProductDTO();
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+				
+		String expectName = productDTO.getName();
+		
+		
+				ResultActions result =  
+						mockMvc.perform(put("/products/{id}", existingId)
+								.content(jsonBody)
+								.contentType(MediaType.APPLICATION_JSON)
+								.accept(MediaType.APPLICATION_JSON));
+				
+				result.andExpect(status().isOk());
+				result.andExpect(jsonPath("$.id").exists());
+				result.andExpect(jsonPath("$.name").value(expectName));
+
+	}	
 
 }
