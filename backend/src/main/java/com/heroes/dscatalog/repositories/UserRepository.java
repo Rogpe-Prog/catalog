512c0b9b24cd701561529b71e45c9fc0f1e5399c
Rogpe-Prog <paulo.rogpe@gmail.com>
2023-02-22 08:06:11 -0300

    implements role

---
+++ backend/src/main/java/com/heroes/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.heroes.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.heroes.dscatalog.entities.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
